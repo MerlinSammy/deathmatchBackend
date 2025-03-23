13bc4eeb679e9c477a67d69dabe32e531d676ecb
Merlin Sammetinger <MSammetinger@ad.jtl-software.de>
2025-03-23 22:05:07 +0100

    Fix

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ app.get('/users', async (req, res) => {
 });
 
 //Get Users der Teilnehmer CSV
+app.get('/', async (req, res) => {
+  res.json("Server Working");
+});
+
+//Get Users der Teilnehmer CSV
 app.get('/dummyData', async (req, res) => {
     try {
         const users = await readCSVFile(csvDummyDataPath);
