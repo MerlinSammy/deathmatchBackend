39cb8b19341220eaef2b7c06dd9104c4e1a296c1
Merlin Sammetinger <MSammetinger@ad.jtl-software.de>
2025-03-23 22:58:44 +0100

    Fix

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/dummyData', async (req, res) => {
 
 //Füge User der Teilnehmer CSV hinzu
 app.post('/addUser', (req, res) => {
-    const { Name, PR, Gewicht } = req.body;
+    const { Name, Gewicht, PR } = req.body;
 
     if (!Name || !PR || !Gewicht) {
         return res.status(400).json({ error: 'Alle Felder (Name, Gewicht, PR) sind erforderlich.' });
